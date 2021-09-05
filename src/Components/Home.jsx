2df3aba00af88e7745746c156926a41914ff995e
Lindos-Mahadi <lindosmahadi@.com>
2021-09-05 11:27:34 +0600

    Add to Cart Design

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import note10 from "../img/note10.jpg"
 
 function Home() {
     
     return (
-        <div>
-            <h1>Home Componet</h1>
+        <React.Fragment>
+            <div>
+            <div className="add-to-cart">
+                <img src="https://static.vecteezy.com/system/resources/thumbnails/000/496/007/small/Ecommerce_998.jpg" />
+            </div>
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    {/* <img src="https://www.fdfproject.com/wp-content/uploads/2018/12/iphone-png.png" /> */}
+                    <img src={note10} />
+                </div>
+                <div className="text-wrapper item">
+                    <span>
+                        I-Phone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button>Add To Cart</button>
+                </div>
+            </div>
         </div>
+        </React.Fragment>
     )
 }
 
